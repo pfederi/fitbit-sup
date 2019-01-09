da3290eff8c3ebe72a7b90812982786d92b7f242
Patrick Federi <patrick@federi.com>
2019-01-09 17:07:04 +0100

    added settings

--- settings/index.jsx
+++ settings/index.jsx
@@ -2,44 +2,27 @@ function mySettings(props) {
   return (
     <Page>
       <Section
-        title={<Text bold align="center">Time Color</Text>}>
-        <ColorSelect
-          settingsKey="timeColor"
-          colors={[
-            {color: "tomato"},
-            {color: "sandybrown"},
-            {color: "#FFFF00"},
-            {color: "#00FFFF"},
-            {color: "deepskyblue"},
-            {color: "plum"}
-          ]}
-        />
+        title={<Text bold align="left">Donate</Text>}>
+        <Link source="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=patrick@supweather.net&item_name=Fitbit+SUP+App+Donation">
+          <TextImageRow
+            label="PayPal"
+            sublabel="patrick@supweather.net"
+            icon="https://pfederi.github.io/resources/paypal.png"
+          />
+        </Link>
       </Section>
       <Section
-        title={<Text bold align="center">Date Settings</Text>}>
-        <Toggle settingsKey="showDate" label="Show date" />
-        <ColorSelect
-          settingsKey="dateColor"
-          colors={[
-            {color: "tomato"},
-            {color: "sandybrown"},
-            {color: "#FFFF00"},
-            {color: "#00FFFF"},
-            {color: "deepskyblue"},
-            {color: "plum"}
-          ]}
-        />
-        <Select
-          label={`Selection`}
-          settingsKey="dateFormat"
-          options={[
-            {name:"dd-mm-yyyy"},
-            {name:"mm-dd-yyyy"},
-            {name:"yyyy-mm-dd"}
-          ]}
-        />
+        title={<Text bold align="left">Contact</Text>}>
+        <Link source="https://pfederi.github.io">
+          <TextImageRow
+            label="Email"
+            sublabel="patrick@federi.com"
+            icon="https://pfederi.github.io/resources/mail.png"
+          />
+        </Link>
       </Section>
-    </Page>);
+    </Page>
+  );
 }
 
 registerSettingsPage(mySettings);
