a6ff2cf786ef14f8e463543714ab61efbf0cba04
Patrick Federi <federi@diggle.ergon.ch>
2019-11-05 07:59:03 +0100

    changes on darksky api

--- companion/index.js
+++ companion/index.js
@@ -4,6 +4,6 @@
  import "fitbit-google-analytics/companion"
  import * as weather from '../modules/fitbit-weather/companion'
 
- weather.setup({ provider : weather.Providers.darksky, apiKey : '3aec0a95dd1d2baf3caf89ca6af74f1b' })
+ weather.setup({ provider : weather.Providers.darksky, apiKey : 'insert darksky api' })
 
 console.log("Companion code started");
